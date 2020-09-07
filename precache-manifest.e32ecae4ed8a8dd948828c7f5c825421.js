d07b056af90f02323873fa76cb28613171ce4544
coraxyc <ycxing99@gmail.com>
2020-09-07 01:12:14 -0700

    Updates

---
+++ precache-manifest.e32ecae4ed8a8dd948828c7f5c825421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95eab1437f5daf01d4b221afb7bcf15",
+    "url": "/the-shoppies-2020/index.html"
+  },
+  {
+    "revision": "b66d9d19769fd66a9bea",
+    "url": "/the-shoppies-2020/static/css/main.fe5086ae.chunk.css"
+  },
+  {
+    "revision": "805e4de82bbc394f06c4",
+    "url": "/the-shoppies-2020/static/js/2.17018321.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-shoppies-2020/static/js/2.17018321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66d9d19769fd66a9bea",
+    "url": "/the-shoppies-2020/static/js/main.3d424934.chunk.js"
+  },
+  {
+    "revision": "33168a2bc53e0d340847",
+    "url": "/the-shoppies-2020/static/js/runtime-main.a1d1f718.js"
+  }
+]);
